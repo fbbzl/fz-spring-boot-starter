efce5a832036aaab8c7431b99794bda60d96576a
fengbinbin <290361638@qq.com>
2026-04-14 14:29:07 +0800

    feat: Added a message module launcher
    - Added the fz-spring-boot-starter-messages module
    - Register the messages module as a child module in the parent project

---
+++ fz-spring-boot-starter-messages/src/main/java/com/fz/starter/messages/util/M.java
@@ -0,0 +1,25 @@
+package com.fz.starter.messages.util;
+
+import cn.hutool.extra.spring.SpringUtil;
+import lombok.experimental.UtilityClass;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+
+/**
+ * message resource util
+ *
+ * @author fengbinbin
+ * @version 1.0
+ * @since 2026/2/14 14:10
+ */
+@UtilityClass
+public class M
+{
+
+    public static String of(String code, Object... args)
+    {
+        MessageSource messageSource = SpringUtil.getBean(MessageSource.class);
+        if (messageSource == null) return code;
+        return messageSource.getMessage(code, args, LocaleContextHolder.getLocale());
+    }
+}
