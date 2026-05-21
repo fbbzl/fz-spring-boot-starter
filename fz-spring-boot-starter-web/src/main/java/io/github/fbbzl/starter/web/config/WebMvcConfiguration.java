4410019b4081075da759c87a1743250f0ac3307b
fengbinbin <290361638@qq.com>
2026-05-21 12:24:28 +0800

    chore: release fz-spring-boot-starter 1.3.44

--- fz-spring-boot-starter-web/src/main/java/io/github/fbbzl/starter/web/config/WebMvcConfiguration.java
+++ fz-spring-boot-starter-web/src/main/java/io/github/fbbzl/starter/web/config/WebMvcConfiguration.java
@@ -1,11 +1,18 @@
 package io.github.fbbzl.starter.web.config;
 
+import cn.crane4j.core.parser.handler.strategy.PropertyMappingStrategyManager;
+import cn.crane4j.core.support.AnnotationFinder;
+import cn.crane4j.core.support.Crane4jGlobalConfiguration;
+import io.github.fbbzl.starter.pojo.crane4j.AssembleBeanAnnotationHandler;
 import io.github.fbbzl.starter.web.advice.WebExceptionAdvice;
 import io.github.fbbzl.starter.web.advice.WebRequestAdvice;
 import io.github.fbbzl.starter.web.advice.WebResponseOperateAdvice;
 import io.github.fbbzl.starter.web.advice.WebResponseWrapAdvice;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 /**
@@ -24,4 +31,19 @@ import org.springframework.context.annotation.Import;
 public class WebMvcConfiguration
 {
 
+    @Bean
+    @ConditionalOnMissingBean(AssembleBeanAnnotationHandler.class)
+    public AssembleBeanAnnotationHandler assembleBeanAnnotationHandler(
+            AnnotationFinder annotationFinder,
+            Crane4jGlobalConfiguration globalConfiguration,
+            PropertyMappingStrategyManager propertyMappingStrategyManager,
+            ApplicationContext applicationContext)
+    {
+        return new AssembleBeanAnnotationHandler(
+                annotationFinder,
+                globalConfiguration,
+                propertyMappingStrategyManager,
+                applicationContext
+        );
+    }
 }
--- fz-spring-boot-starter-webflux/src/main/java/io/github/fbbzl/starter/webflux/config/WebFluxConfiguration.java
+++ fz-spring-boot-starter-webflux/src/main/java/io/github/fbbzl/starter/webflux/config/WebFluxConfiguration.java
@@ -1,10 +1,15 @@
 package io.github.fbbzl.starter.webflux.config;
 
+import cn.crane4j.core.parser.handler.strategy.PropertyMappingStrategyManager;
+import cn.crane4j.core.support.AnnotationFinder;
+import cn.crane4j.core.support.Crane4jGlobalConfiguration;
 import cn.crane4j.core.support.OperateTemplate;
+import io.github.fbbzl.starter.pojo.crane4j.AssembleBeanAnnotationHandler;
 import io.github.fbbzl.starter.webflux.advice.WebExceptionAdvice;
 import io.github.fbbzl.starter.webflux.advice.WebFluxResponseBodyResultHandler;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
@@ -22,6 +27,22 @@ import org.springframework.web.reactive.accept.RequestedContentTypeResolver;
 @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
 public class WebFluxConfiguration
 {
+
+    @Bean
+    @ConditionalOnMissingBean(AssembleBeanAnnotationHandler.class)
+    public AssembleBeanAnnotationHandler assembleBeanAnnotationHandler(
+            AnnotationFinder annotationFinder,
+            Crane4jGlobalConfiguration globalConfiguration,
+            PropertyMappingStrategyManager propertyMappingStrategyManager,
+            ApplicationContext applicationContext)
+    {
+        return new AssembleBeanAnnotationHandler(
+                annotationFinder,
+                globalConfiguration,
+                propertyMappingStrategyManager,
+                applicationContext
+        );
+    }
 
     @Bean
     public WebFluxResponseBodyResultHandler fzWebFluxResponseBodyResultHandler(
